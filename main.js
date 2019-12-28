d24e088cc6a7595e7bb5ef658d4af861081b6108
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2019-12-28 19:14:35 +0100

    handle formData

--- main.js
+++ main.js
@@ -2,33 +2,45 @@ const $ = require('jquery')
 
 let Ajax = {}
 
-Ajax.request = (url, params, dataType, method) => {
-    return {
+Ajax.request = (url, params, dataType, method, isformData = false) => {
+    let config = {
         url : url,
         method : method,
         data : params,
         dataType : dataType
     }
+
+    if (isformData) {
+        let formData = new FormData()
+        for (const [key, value] of Object.entries(params)) {
+            formData.append(key, value)
+        }
+        config.data = formData
+        config.contentType = false
+        config.processData = false
+    }
+
+    return config
 }
 
-Ajax.post = (url, params = {}, dataType = 'json') => {
+Ajax.post = (url, params = {}, dataType = 'json', isformData = false) => {
     return new Promise ((resolve, reject) => {
-        $.ajax(Ajax.request(url, params, dataType, 'POST'))
+        $.ajax(Ajax.request(url, params, dataType, 'POST', isformData))
             .done(resolve)
             .fail(reject)
     })
 }
 
-Ajax.get = (url, params = {}, dataType = 'json') => {
+Ajax.get = (url, params = {}, dataType = 'json', isformData = false) => {
     return new Promise ((resolve, reject) => {
-        $.ajax(Ajax.request(url, params, dataType, 'GET'))
+        $.ajax(Ajax.request(url, params, dataType, 'GET', isformData))
             .done(resolve)
             .fail(reject)
     })
 }
 
-Ajax.callDataUrl = (elt, params = {}, dataType = 'json') => {
-    return Ajax.get($(elt).data('url'), params, dataType)
+Ajax.callDataUrl = (elt, params = {}, dataType = 'json', isformData = false) => {
+    return Ajax.get($(elt).data('url'), params, dataType, isformData)
 }
 
 module.exports = Ajax
